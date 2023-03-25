cce65744ce703ac6f22a2b6ba559f2a7eb3df87d
Dan <109627357+DanInTheGitHub@users.noreply.github.com>
2023-03-25 05:07:11 -0500

    Update (Functional)

--- Assets/Scripts/Dan/BulletSetting.cs
+++ Assets/Scripts/Dan/BulletSetting.cs
@@ -4,56 +4,20 @@ using UnityEngine;
 
 public class BulletSetting : MonoBehaviour
 {
-    Rigidbody rb;
+    public float speed = 10f;
+    public float lifetime = 5f;
 
-    float destroyTime;
-
-    public int damage = 1;
-
-    [SerializeField] float bulletSpeed = 0.3f;
-    [SerializeField] Vector2 bulletDirection;
-    [SerializeField] float destroyDelay = 0.7f;
-
-    // Start is called before the first frame update
-    void Awake()
+    private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        Destroy(gameObject, lifetime);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        destroyTime -= Time.deltaTime;
-        if (destroyTime < 0)
-        {
-            Destroy(gameObject);
-        }
-    }
-    public void SetBulletSpeed(float speed)
-    {
-        this.bulletSpeed = speed;
-    }
-    public void SetBulletDirection(Vector2 direction)
-    {
-        this.bulletDirection = direction;
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
     }
 
-    public void SetDamageValue(int damage)
-    {
-        this.damage = damage;
-    }
-    public void SetBulletDelay(float delay)
-    {
-        this.destroyDelay = delay;
-    }
-
-    public void Shoot()
-    {
-        rb.velocity = bulletDirection * bulletSpeed;
-        destroyTime = destroyDelay;
-    }
-
-    void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
     }
--- Assets/Scripts/Dan/DirectionalShoting.cs
+++ Assets/Scripts/Dan/DirectionalShoting.cs
@@ -1,90 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DirectionalShoting : MonoBehaviour
 {
-    [Header("Joystick Settings")]
-    [SerializeField] public Joystick joystick;
-    [SerializeField][Range(0f, 1f)] public float deathZoneX;
-    [SerializeField][Range(0f, 1f)] public float deathZoneJumpY;
-    [SerializeField][Range(0f, 1f)] public float deathZoneCrouchY;
+    public Joystick joystickB;
+    public GameObject projectilePrefab;
+    public float shootDelay = 0.5f;
+    private float lastShootTime = 0f;
 
-    [Header("Bullet Settings")]
-    [SerializeField] int bulletDamage = 1;
-    [SerializeField] float bulletSpeed = 5f;
-    [SerializeField] Transform gunPos;
-    [SerializeField] GameObject bulletPrefab;
-
-    bool keyShot;
-
-    bool isShooting;
-
-    bool keyShotReleace;
-    float shorttime;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        BulletDirectionInput();
-        PlayerShootInput();
-
-        //keyHorizon = Input.GetAxisRaw("Horizontal");
-    }
-
-    void BulletDirectionInput()
-    {
-        //keyHorizon = Input.GetAxisRaw("Horizontal");
-        //keyJump = Input.GetKeyDown(KeyCode.Space);
-        //keyShot = Input.GetKey(KeyCode.F);
-    }
-
-    void PlayerShootInput()
-    {
-        float shootTimeLength = 0;
-        float keyShootReleaseTimeLength = 0;
-
-        if (keyShot && keyShotReleace)
+        if (joystickB.Horizontal != 0f || joystickB.Vertical != 0f)
         {
-            isShooting = true;
-            keyShotReleace = false;
-            shorttime = Time.time;
-
-            Debug.Log("Shoot Bullet");
-            ShootBullet();
-        }
-
-        if (!keyShot && !keyShotReleace)
-        {
-            keyShootReleaseTimeLength = Time.time - shorttime;
-            keyShotReleace = true;
-        }
-
-        if (isShooting)
-        {
-            shootTimeLength = Time.time - shorttime;
-            if (shootTimeLength >= 0.25f || keyShootReleaseTimeLength >= 0.15f)
+            float angle = Mathf.Atan2(joystickB.Vertical, joystickB.Horizontal) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle - 90f));
+            if (Time.time - lastShootTime > shootDelay)
             {
-                isShooting = false;
+                Shoot();
+                lastShootTime = Time.time;
             }
         }
     }
 
-    void ShootBullet()
+    private void Shoot()
     {
-        GameObject bullet = Instantiate(bulletPrefab, gunPos.position, Quaternion.identity);
-
-        bullet.name = bulletPrefab.name;
-
-        bullet.GetComponent<BulletSetting>().SetDamageValue(bulletDamage);
-        bullet.GetComponent<BulletSetting>().SetBulletSpeed(bulletSpeed);
-        //bullet.GetComponent<BulletSetting>().SetBulletDirection();
-        bullet.GetComponent<BulletSetting>().Shoot();
+        Vector3 spawnPosition = transform.position + transform.up * 1.0f;
+        GameObject projectile = Instantiate(projectilePrefab, spawnPosition, transform.rotation);
     }
 }
