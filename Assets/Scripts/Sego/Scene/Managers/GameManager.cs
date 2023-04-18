ed29c56ff871a043a21c5ccc879750c30fcfac28
Segoo <68073260+Michikatsu0@users.noreply.github.com>
2023-04-17 22:22:33 -0500

    Trash

--- Assets/Scripts/Sego/Scene/Managers/GameManager.cs
+++ Assets/Scripts/Sego/Scene/Managers/GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public enum SceneIndexes { TITLE, LOADING, PROTOTYPING }
+public enum SceneIndexes { TITLE, ZONE, LOADING,PROTOTYPING }
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
